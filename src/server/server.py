f0e890d9d6c6d82711d9b985846898d4a3af9af0
Brianna Mattia <bmattia@briannas-mbp.lan>
2020-03-06 21:20:49 -0600

    add endpoints for character names

--- src/server/server.py
+++ src/server/server.py
@@ -11,6 +11,18 @@ game_state = GameState.from_rom_constants()
 def index():
     return jsonify(game_state.get_features())
 
+@app.route('/npcs')
+def npcs():
+    return jsonify(['baby', 'bartender', 'basil', 'carpenter_axe', 'carpenter_master', 'carpenter_saw', 'doug', 'ellen', 'fisherman', 'gotz', 'gotz_wife', 'gourmet', 'kappa', 'kent', 'lillia', 'may', 'mayor', 'mayor_wife', 'midwife', 'old_man', 'old_woman', 'pastor', 'potion_master', 'rick', 'saibara', 'shipper', 'sprite_1', 'sprite_2', 'sprite_3', 'stu'])
+
+@app.route('/girls')
+def girls():
+    return jsonify(['ann', 'elli', 'karen', 'maria', 'popuri'])
+
+@app.route('/rivals')
+def rivals():
+    return jsonify(['cliff', 'gray', 'harris', 'jeff', 'kai'])
+
 @app.route('/favicon.ico')
 def favicon():
     return send_from_directory(
